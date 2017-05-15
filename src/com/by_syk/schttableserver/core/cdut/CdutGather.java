538726e9e7d4186404b657f788bab29452147aca
By_syk <By_syk@163.com>
2017-05-15 11:38:23 +0800

    cdut highlight

--- src/com/by_syk/schttableserver/core/cdut/CdutGather.java
+++ src/com/by_syk/schttableserver/core/cdut/CdutGather.java
@@ -18,6 +18,7 @@ public class CdutGather extends BaseGather {
     protected boolean pack(File zipFile, String coursePage) {
         System.out.println("CdutGather - pack");
         
+        coursePage = highlight(coursePage);
         return ExtraUtil.zip(zipFile, new String[]{COURSE_PAGE_FILE_NAME},
                 new String[]{coursePage});
     }
@@ -28,7 +29,7 @@ public class CdutGather extends BaseGather {
 //        }
 //        
 //        Calendar calendar = Calendar.getInstance();
-//        int courses_start =  ((7 + calendar.get(Calendar.DAY_OF_WEEK) - 2) % 7) * 12 + 1;
+//        int courses_start = ((7 + calendar.get(Calendar.DAY_OF_WEEK) - 2) % 7) * 12 + 1;
 //        int courses_end =  courses_start + 11;
 //        
 //        // 回到周一
@@ -60,4 +61,50 @@ public class CdutGather extends BaseGather {
 //        
 //        return coursePage;
 //    }
+    
+    private String highlight(String coursePage) {
+        if (coursePage == null) {
+            return null;
+        }
+        
+        String script = "<script type=\"text/javascript\">"
+                + "\nvar d = new Date();"
+                + "\nvar courseStart = ((d.getDay() + 6) % 7) * 12 + 1;"
+                + "\nvar courseEnd =  courseStart + 11;"
+                + "\nvar decDays = d.getDay();"
+                + "\nif (decDays == 0) {"
+                + "\n  decDays == 7;"
+                + "\n}"
+                + "\nd.setDate(d.getDate() - (d.getDay() + 6) % 7);"
+                + "\nvar keyword = format(d.getMonth() + 1) + '/' + format(d.getDate());"
+                + "\nvar tds = document.getElementsByClassName('td1');"
+                + "\nfor (var i in tds) {"
+                + "\n  var content = tds[i].innerHTML;"
+                + "\n  if (content && content.indexOf('>' + keyword + '-') >= 0) {"
+                + "\n    var courseCount = 0;"
+                + "\n    tds = tds[i].parentNode.childNodes;"
+                + "\n    for (i in tds) {"
+                + "\n      if (tds[i].tagName == 'TD' && tds[i].className == 'fontcss') {"
+                + "\n        var colspan = tds[i].getAttribute('colspan');"
+                + "\n        courseCount += (colspan ? parseInt(colspan) : 1);"
+                + "\n        if (courseCount >= courseStart && courseCount <= courseEnd) {"
+                + "\n          tds[i].setAttribute('bgcolor', '#dddddd');"
+                + "\n        }"
+                + "\n      }"
+                + "\n    }"
+                + "\n    break;"
+                + "\n  }"
+                + "\n}"
+                + "\nfunction format(digit) {"
+                + "\n  return (digit < 10 ? '0' : '') + digit;"
+                + "\n}"
+                + "\n</script>\n\n";
+        String insertKeyword = "</body>";
+        int index = coursePage.indexOf(insertKeyword);
+        if (index > 0) {
+            coursePage = coursePage.replace(insertKeyword, script + insertKeyword);
+        }
+        
+        return coursePage;
+    }
 }
